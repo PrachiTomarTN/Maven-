0e91254672a47f151ec0b6ac03b090f5a2e9583a
prachi <prachi.tomar@tothenew>
2022-10-14 09:31:29 +0530

    First Commit

---
+++ src/test/java/org/example/UnitTest.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class UnitTest {
+    public class unittest
+    {
+        public static void main(String[] args) {
+            System.out.println("Hello World");
+        }
+    }
+}
